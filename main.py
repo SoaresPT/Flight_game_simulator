5fe31f929c120e6bdf2e42c748bfbebc0f6d4f54
Soares <soares_123@msn.com>
2022-10-08 05:12:18 +0300

    Rename and fix some functions. Made a sample looping.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from geopy import distance
 """
 Basic Connection Functions
 """
+
 def config(filename='database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
@@ -60,7 +61,7 @@ def get_random_airports():
         cur.execute(sql_db_length)
         result = cur.fetchall()
         # Generate X number of random airports. Make sure it's not a repeated one and different than the starting location
-        if result[0] not in airports_list and result[0][0] != current_city[-1]:
+        if result[0] not in airports_list and result[0][0] != current_location[-1]:
             airports_tuple = (result[0][0], result[0][1])
         else:
             continue
@@ -70,18 +71,18 @@ def get_random_airports():
 # Iterate and print the list of airports the user must travel to. This outputs in a nicer format the contents of get_random_airports()
 def print_airports(airport_list: list):
     for index, tup in enumerate(airport_list):
-        print(f"{tup[0]}, {tup[1]}")
+        print(f"\t{tup[0]}, {tup[1]}")
 
 # Find airports nearby
 def airports_nearby():
-    flight_range = 400
+    flight_range = 800
     reachable_airports = []
     # for i in range(airport_table_length):
-    nearby = f"SELECT * from airport where city != '{current_city[-1]}';"  # [(52.3086, 4.76389), (59.4133, 24.8328), (48.1103, 16.5697), (44.0203, 12.6117), (47.4298, 19.2611), (49.0128, 2.55), (43.8246, 18.3315), (55.9726, 37.4146), (52.3514, 13.4939), (45.7429, 16.0688), (52.1657, 20.9671), (50.9014, 4.48444), (38.7813, -9.13592), (35.8575, 14.4775), (55.6179, 12.656), (43.7258, 7.41967), (42.3594, 19.2519), (41.8045, 12.252), (41.9616, 21.6214), (60.1939, 11.1004), (53.8881, 28.04), (53.4213, -6.27007), (49.6233, 6.20444), (42.6967, 23.4114), (59.6519, 17.9186), (42.3386, 1.40917), (56.9236, 23.9711), (37.9364, 23.9445), (44.5711, 26.085), (46.9277, 28.931), (64.13, -21.9406), (50.1008, 14.26), (48.1702, 17.2127), (44.8184, 20.3091), (54.6341, 25.2858), (51.5053, 0.055278), (50.345, 30.8947), (41.4147, 19.7206), (46.9141, 7.49715), (46.2237, 14.4576), (40.4719, -3.56264)]
+    nearby = f"SELECT * from airport where city != '{current_location[-1]}';"  # [(52.3086, 4.76389), (59.4133, 24.8328), (48.1103, 16.5697), (44.0203, 12.6117), (47.4298, 19.2611), (49.0128, 2.55), (43.8246, 18.3315), (55.9726, 37.4146), (52.3514, 13.4939), (45.7429, 16.0688), (52.1657, 20.9671), (50.9014, 4.48444), (38.7813, -9.13592), (35.8575, 14.4775), (55.6179, 12.656), (43.7258, 7.41967), (42.3594, 19.2519), (41.8045, 12.252), (41.9616, 21.6214), (60.1939, 11.1004), (53.8881, 28.04), (53.4213, -6.27007), (49.6233, 6.20444), (42.6967, 23.4114), (59.6519, 17.9186), (42.3386, 1.40917), (56.9236, 23.9711), (37.9364, 23.9445), (44.5711, 26.085), (46.9277, 28.931), (64.13, -21.9406), (50.1008, 14.26), (48.1702, 17.2127), (44.8184, 20.3091), (54.6341, 25.2858), (51.5053, 0.055278), (50.345, 30.8947), (41.4147, 19.7206), (46.9141, 7.49715), (46.2237, 14.4576), (40.4719, -3.56264)]
     cur.execute(nearby)
     result = cur.fetchall()
     for coords in result:
-        if distance.distance(coords[2:4], current_city[2:4]).km < flight_range:
+        if distance.distance(coords[2:4], current_location[2:4]).km < flight_range:
             reachable_airports.append(coords)
     return reachable_airports
 
@@ -95,22 +96,22 @@ def flight_target(airports: list):
     return target_city
 
 def update_curr_location():
-    current = f"UPDATE player SET curr_location = '{flight_target()[-1]}' WHERE username LIKE '%Sergio%';"
+    current = f"UPDATE player SET curr_location = '{current_city_country}' WHERE username = '{username}';"
     cur.execute(current)
-    get_info = f"SELECT * from player WHERE username = 'Sergio';"
+    get_info = f"SELECT * from player WHERE username = '{username}';"
     cur.execute(get_info)
     conn.commit()
     res = cur.fetchall()
     print(res)
     return res
 
-def default_city():
-    global current_city
+def starting_location():
+    global current_location
     start_city_query = f"SELECT * FROM airport WHERE icao = 'EFHK';"
     cur.execute(start_city_query)
     result = cur.fetchall()
-    current_city = result[0]
-    return current_city
+    current_location = result[0]
+    return current_location
 
 def closeDBConnection():
     try:
@@ -137,7 +138,7 @@ def add_username(username: str):
         add_new_user = f"INSERT INTO player(username) VALUES ('{username}')"
         cur.execute(add_new_user)
         conn.commit()
-        return f"{username} has been added to the database!"
+        print(f"{username} has been added to the database!")
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
@@ -181,15 +182,32 @@ if __name__ == "__main__":
     else:
         print(f"Welcome back, {username}!")
     
-    # Populate the current_city global var -- TO DO: Change this as it's clunky af
-    default_city()
-
+    # Populate the current_location - Currently will always be Helsinki
+    current_location = starting_location()
+    current_city_country = f"{current_location[-1]}, {current_location[-2]}"
+    print(f"Your mission is to deliver packages to the following airports. You starting position is {current_city_country}. Good luck!")
+    print(f"Reach these airports by any order:")
     # Grab the 5 closest airports to the current location
     generated_5_airports = get_random_airports()
     print_airports(generated_5_airports)
     print("\n")
-    #close_airports = airports_nearby()
-    #print(flight_target(close_airports))
 
+    print("You are flying a small place and so you have limited fuel. You can travel to any of these airports to refuel:")
+
+    while len(generated_5_airports) > 0:
+        nearby_airports = airports_nearby()
+        destination = flight_target(nearby_airports)
+        current_city_country = f"{destination[-1]}, {destination[-2]}"
+        current_location = destination
+        update_curr_location()
+        print(f"You're now in {current_city_country}")
+
+
+    
     # forcefully closing this to be 100% sure no connection gets stuck 
     closeDBConnection()
+    """
+    This will remove a city, country from the list of tuples - TO DO LATER
+    """
+    #generated_5_airports.remove(("Vienna", "Austria"))
+    #print(generated_5_airports)
